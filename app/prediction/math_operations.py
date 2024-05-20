95d4fc1ec8d39bcf4b56b9f62ca042b26b270123
AronW <64489382+AronWichtner@users.noreply.github.com>
2024-05-20 15:52:40 +0200

    implementing parser for wiggle files and creating small test wiggle file

--- app/prediction/math_operations.py
+++ app/prediction/math_operations.py
@@ -1,14 +1,38 @@
 import numpy as np
 import wiggelen as wig
-import pandas as p
+import pandas as pd
+import os
+
 
 # this file contains useful mathematical methods for the work with wiggle files
 
 
-# example; parsing wiggle file
-#...
-# create pandas DataFrame
-#...
+# example; parsing wiggle file:
+
+wiggle = os.path.relpath('..\\..\\tests\\test_files\\test.wig')
+def parse_wiggle_to_DataFrame(wiggle):
+    """
+    parses a wiggle file to a pandas DataFrame
+    :param wiggle: path to a wiggle file
+    :return: pandas DataFrame
+    """
+    lst_of_triple = []
+
+    for region, position, value in wig.fill(wig.walk(open(wiggle))):
+        lst_of_triple.append((region, position, value))
+
+    print(lst_of_triple)
+    return pd.DataFrame(lst_of_triple)
+
+# print(parse_wiggle_to_DataFrame(wiggle))
+
+
+# Aron
+# Addition
+# Subtraktion
+# Multiplikation
+# Division
+
 
 # Amelie
 # Median
@@ -16,8 +40,4 @@ import pandas as p
 # Mean
 # Standartabweichung
 
-# Aron
-# Addition
-# Subtraktion
-# Multiplikation
-# Division
+
