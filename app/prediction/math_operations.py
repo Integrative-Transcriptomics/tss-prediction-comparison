3ef1866205ae229e44f960bd8af0c1fb311c3b0e
AronW <64489382+AronWichtner@users.noreply.github.com>
2024-05-20 14:25:49 +0200

    updating requirements and creating math_operations.py

---
+++ app/prediction/math_operations.py
@@ -0,0 +1,23 @@
+import numpy as np
+import wiggelen as wig
+import pandas as p
+
+# this file contains useful mathematical methods for the work with wiggle files
+
+
+# example; parsing wiggle file
+#...
+# create pandas DataFrame
+#...
+
+# Amelie
+# Median
+# Quantil
+# Mean
+# Standartabweichung
+
+# Aron
+# Addition
+# Subtraktion
+# Multiplikation
+# Division
