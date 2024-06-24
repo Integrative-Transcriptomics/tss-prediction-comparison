8654dcd593e5341604b1cbe61aa863058abd0d10
Jenniksen <jannik.kaufmann23@gmail.com>
2024-06-25 00:08:07 +0200

    added a button that leads to visulization page

--- client-side-interface-prototype/src/JobManagement.jsx
+++ client-side-interface-prototype/src/JobManagement.jsx
@@ -1,6 +1,6 @@
 // JobManagement.jsx
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function JobManagement() {
   // to access the projectName we access the passed state from the first page with useLocation
@@ -8,12 +8,20 @@ function JobManagement() {
   // if the projectName is undefined for whatever reason the default projectName is empty
   const { projectName } = location.state || { projectName: 'Default Project Name' };
 
+  const navigatetoVisualization = useNavigate();
+
+  const handleVisualization = () => {
+    navigatetoVisualization('/visualization');
+  }
+
   return (
     <div>
       <h1>Job Management</h1>
       <p>Project Name: {projectName}</p>
+      <button onClick={handleVisualization}>Go to Visualization</button>
     </div>
   );
 }
 
 export default JobManagement;
+
---
+++ client-side-interface-prototype/src/Visualization.jsx
@@ -0,0 +1,13 @@
+// Visualization.jsx
+import React from 'react';
+
+function Visualization() {
+  return (
+    <div>
+      <h1>Visualization Page</h1>
+      <p>This is a placeholder for visualization content.</p>
+    </div>
+  );
+}
+
+export default Visualization;
