49d732de3b6188d41ea7cda18a5eceaea6dce2bd
M. Witte Paz <mathiaswittepaz@gmail.com>
2024-06-26 08:39:18 +0200

    Adapts api configuration to connect vite and flask Adds API identifier before every call Proxys the server of vite to connect to flask

--- app/server.py
+++ app/server.py
@@ -45,7 +45,7 @@ def get_job_by_id(id_):
 
 # Upload endpoint. Checks uploaded file for wiggle file ending, stores file, creates job object and returns job id
 # upon sucess
-@app.route("/upload", methods=["POST"])
+@app.route("/api/upload", methods=["POST"])
 def upload_file():
     if request.method == 'POST':
 
@@ -111,7 +111,7 @@ def upload_file():
 
 
 # get file by id endpoint. Returns the wiggle file if a job with given id exists
-@app.route("/get_file", methods=["GET"])
+@app.route("/api/get_file", methods=["GET"])
 def get_wiggle_by_id():
     if request.method == 'GET':
         id = request.args.get('jobid', type=str)
@@ -137,7 +137,7 @@ def get_wiggle_by_id():
 
 
 # gets job state by id. Returns job state ("Not started", "Running", "Finished", "Failed") if job exists
-@app.route("/get_state", methods=["GET"])
+@app.route("/api/get_state", methods=["GET"])
 def get_job_state_by_id():
     if request.method == 'GET':
         id = request.args.get('jobid', type=str)
@@ -153,7 +153,7 @@ def get_job_state_by_id():
 
 
 # gets tss prediction by id. Fails if Job state is not finished.
-@app.route("/get_tss", methods=["GET"])
+@app.route("/api/get_tss", methods=["GET"])
 def get_tss_by_id():
     if request.method == 'GET':
         id = request.args.get('jobid', type=str)
