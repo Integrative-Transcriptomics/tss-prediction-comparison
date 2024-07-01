eb3e940159511944f1934d02e35308533b405668
Amelie <amelie.flietel@student.uni-tuebingen.de>
2024-07-01 13:17:51 +0200

    add gene names

--- app/TSSclassifier.py
+++ app/TSSclassifier.py
@@ -1,7 +1,11 @@
+import pydoc
+
 import pandas as pd
 from enum import Enum
 from json import loads, dumps
 from app import GFFParser as ps
+import re
+
 
 class TSSType(Enum):
     PRIMARY = "pTSS/sTSS"
@@ -10,11 +14,19 @@ class TSSType(Enum):
     ORPHAN = "orphan"
 
 
-def classify(gff_df, TSS_dict, strand):
-    #gff_df = ps.parse_gff_to_df(gff_df)
+# erwartet String, ab Name= bis zum nächsten semikolon
+def extract_name(attributes):
+    match = re.search(r'Name=([^;]+)', attributes)
+    return match.group(1) if match else None
 
+
+def classify(gff_df, TSS_dict, strand):
+    gff_df = ps.parse_gff_to_df(gff_df)
 
     tss_classified = {}
+    gff_df['gene name'] = gff_df['attributes'].apply(extract_name)
+
+    gene_name = []
 
     gff_fw = gff_df[(gff_df['strand'] == "+")]
     gff_rv = gff_df[(gff_df['strand'] == "-")]
@@ -34,22 +46,27 @@ def classify(gff_df, TSS_dict, strand):
         possible_class = []
 
         if not intern.empty:
-            possible_class.append(TSSType.INTERNAL.value)
+            possible_class.extend([TSSType.INTERNAL.value] * len(intern))
+            gene_name.extend(intern['gene name'].tolist())
 
         if not prim.empty:
-            possible_class.append(TSSType.PRIMARY.value)
+            possible_class.extend([TSSType.PRIMARY.value] * len(prim))
+            gene_name.extend(prim['gene name'].tolist())
 
         if not anti.empty:
-            possible_class.append(TSSType.ANTISENSE.value)
+            possible_class.extend([TSSType.ANTISENSE.value] * len(anti))
+            gene_name.extend(anti['gene name'].tolist())
 
         if len(possible_class) == 0:
             possible_class = [TSSType.ORPHAN.value]
+            gene_name.append(None)
 
         tss_classified[tss] = possible_class
 
     data_tuples = [(k, tss_type) for k, v in tss_classified.items() for tss_type in v]
 
     tss_classified = pd.DataFrame(data_tuples, columns=['pos', 'TSS type'])
+    tss_classified['gene name'] = gene_name
 
     return tss_classified
 
@@ -63,8 +80,6 @@ def find_common_tss(prediction, master_table, strand):
 
     mt_relevant_columns = mt_relevant_columns[['pos', 'TSS type']].reset_index(drop=True)
 
-    #common_tss_df = pd.merge(prediction, mt_relevant_columns, on=['TSS site', 'TSS type'], how='inner')
-
     expanded_prediction = pd.concat([
         prediction.assign(**{'pos': prediction['pos'] + offset})
         for offset in range(-5, 6)
@@ -76,9 +91,14 @@ def find_common_tss(prediction, master_table, strand):
     return common_tss_df
 
 
-def to_csv(df1, df2, master_table):
+def to_csv(prediction, common_tss, master_table, tss_list, confidence_list):
+    df = pd.DataFrame({'pos': tss_list, 'confidence': confidence_list})
+    prediction = pd.merge(df, prediction, on='pos', how='inner')
+    columns_order = ['pos', 'TSS type', 'confidence', 'gene name']
+    prediction = prediction[columns_order]
+
     mt_relevant_columns = master_table[['pos', 'TSS type']]
-    dfs = [(df1, 'prediction'), (df2, 'shared_TSS'), (mt_relevant_columns, 'TSS_predator')]
+    dfs = [(prediction, 'prediction'), (common_tss, 'shared_TSS'), (mt_relevant_columns, 'TSS_predator')]
 
     for df, name in dfs:
         csv_file = f'{name}.csv'
@@ -111,7 +131,7 @@ def freq_return_obj(common_tss_df, prediction_df, master_table_df):
     result_json = loads(dumps(result_dict))
     return result_json
 
-'''
+
 mt = {
     'pos': [25675, 31366, 31650, 32000, 405, 400],
     'TSS type': ["iTSS", "asTSS", "pTSS/sTSS", "asTSS", "iTSS", "pTSS/sTSS"],
@@ -126,7 +146,7 @@ cl = classify("../tests/test_files/NC_004703.gff", [400, 25675, 31362, 31650], F
 print(cl)
 co = find_common_tss(cl, df, False)
 print(co)
-to_csv(cl, co, df)
+to_csv(cl, co, df, [400, 25675, 31362, 31650], [0.9, 0.5, 0.7, 0.8])
 
 print(freq_return_obj(cl, co, df))
-print(recall_and_precision_return_obj(co, cl, df))'''
+print(recall_and_precision_return_obj(co, cl, df))
