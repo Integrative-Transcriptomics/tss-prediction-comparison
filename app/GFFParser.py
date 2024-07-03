bbbbc708ad561d98b98acfb6f5391455018965a9
Amelie <amelie.flietel@student.uni-tuebingen.de>
2024-07-03 09:18:20 +0200

    add documentation

--- app/GFFParser.py
+++ app/GFFParser.py
@@ -2,17 +2,12 @@ import pandas as pd
 
 
 def parse_gff_to_df(filepath):
-    df = pd.read_csv(filepath, sep='\t', comment='#', header=None,
-                     names=["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"])
+    """
+    parses the entries of type gene from a gff file into a data frame
+    :param filepath: path to the gff file
+    :return: df
+    """
+    column_names = ["seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
+    df = pd.read_csv(filepath, sep='\t', comment='#', header=None,names=column_names)
     df = df[df['type'] == 'gene'].reset_index(drop=True)
     return df
-
-
-filepath = "../tests/test_files/NC_004703.gff"
-
-# Display the first few rows
-pd.set_option('display.max_colwidth', None)
-pd.set_option('display.max_columns', None)
-
-if __name__ == '__main__':
-    print(parse_gff_to_df(filepath).head())
