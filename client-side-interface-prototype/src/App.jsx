1c299b81a862c1db91a0db45e7abaa206d4a90e6
Gregor Habitzreither <gregor.habitzreither@student.uni-tuebingen.de>
2024-07-08 22:47:58 +0200

    visualization experiment

--- client-side-interface-prototype/src/App.jsx
+++ client-side-interface-prototype/src/App.jsx
@@ -1,8 +1,8 @@
-// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProjectForm from './ProjectForm';
 import JobManagement from './JobManagement';
+import Visualization from './Visualization';
 import './App.css';
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<ProjectForm />} />
           <Route path="/job-management" element={<JobManagement />} />
+          <Route path="/visualization" element={<Visualization />} />
         </Routes>
       </div>
     </Router>
